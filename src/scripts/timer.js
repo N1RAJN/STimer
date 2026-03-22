f7e13601920ae0ca9cac9401c3e9af5f32264604
Nirajan <nirajanacharya787@gmail.com>
2026-03-22 15:33:13 +0545

    Move SVG paths to another file

--- src/scripts/timer.js
+++ src/scripts/timer.js
@@ -1,4 +1,6 @@
 import { saveSessionInfo, savePauseInfo } from "./sessions.js";
+import { SVGPaths } from "./svgs.js";
+
 const timerContainer = document.getElementById("timerContainer");
 const timerToggleButton = document.getElementById("timerToggleButton");
 const timerStopButton = document.getElementById("timerStopButton");
@@ -30,11 +32,6 @@ export var sessionInfo = {
     description: "", // What I did specifically (eg backtracking, problem name)
     tag: "", // What kind of work did I spend my time on (eg DSA)
     resources: "", // Resources that I used (eg link of the problem i solved, solutions i may have used)
-};
-
-const SVGPaths = {
-    Pause: "M5.92 24.096q0 0.832 0.576 1.408t1.44 0.608h4.032q0.832 0 1.44-0.608t0.576-1.408v-16.16q0-0.832-0.576-1.44t-1.44-0.576h-4.032q-0.832 0-1.44 0.576t-0.576 1.44v16.16zM18.016 24.096q0 0.832 0.608 1.408t1.408 0.608h4.032q0.832 0 1.44-0.608t0.576-1.408v-16.16q0-0.832-0.576-1.44t-1.44-0.576h-4.032q-0.832 0-1.408 0.576t-0.608 1.44v16.16z",
-    Play: "M5.92 24.096q0 1.088 0.928 1.728 0.512 0.288 1.088 0.288 0.448 0 0.896-0.224l16.16-8.064q0.48-0.256 0.8-0.736t0.288-1.088-0.288-1.056-0.8-0.736l-16.16-8.064q-0.448-0.224-0.896-0.224-0.544 0-1.088 0.288-0.928 0.608-0.928 1.728v16.16z",
 };
 
 const sessionTimer = () => {
