c2d6a742ad015302cf5138404b5cbe42e99a6a9a
Nirajan <nirajanacharya787@gmail.com>
2026-05-07 18:47:20 +0545

    Add and remove event listeners on SessionTagCard correctly; Put event handlers in functions

--- src/scripts/components/settings.js
+++ src/scripts/components/settings.js
@@ -18,6 +18,21 @@ import {
 
 import { state, globals } from "../state.js";
 
+function displaySettingsModal() {
+    if (state.timerStarted) return;
+    if (state.stopwatchMode) {
+        countdownDurationContainer.style.display = "none";
+        timerModeDropdown.value = "Stopwatch";
+    } else {
+        countdownDurationContainer.style.display = "flex";
+        timerModeDropdown.value = "Countdown";
+    }
+    countdownDurationMinutes.value =
+        localStorage.getItem("countDownDurationMin") ?? "60";
+    countdownDurationSeconds.value =
+        localStorage.getItem("countDownDurationSec") ?? "00";
+    settingsModal.showModal();
+}
 function hideSessionTagInput() {
     addSessionTagSettingButton.innerHTML = "+";
     addSessionTagSettingInput.style.visibility = "hidden";
@@ -28,6 +43,30 @@ function toggleCountdownDurationContainer() {
         countdownDurationContainer.style.display = "flex";
     } else {
         countdownDurationContainer.style.display = "none";
+    }
+}
+
+function showAddSettingTagsInput() {
+    const buttonText = addSessionTagSettingButton.innerHTML;
+    if (buttonText == "+") {
+        addSessionTagSettingButton.innerHTML = "x";
+        addSessionTagSettingInput.style.visibility = "visible";
+        addSessionTagSettingInput.focus();
+    } else {
+        hideSessionTagInput();
+    }
+}
+
+function addSettingTags(e) {
+    if (e.key == "Enter") {
+        const tagDiv = document.createElement("div");
+        tagDiv.innerHTML = addSessionTagSettingInput.value;
+        tagDiv.className = "Session-Tag-Card";
+        sessionTagsList.appendChild(tagDiv);
+        tagsListSettings.appendChild(tagDiv.cloneNode(true));
+        addSessionTagSettingButton.innerHTML = "+";
+        addSessionTagSettingInput.style.visibility = "hidden";
+        addSessionTagSettingInput.value = "";
     }
 }
 
@@ -52,22 +91,70 @@ function showContextMenu(e) {
         globals.selectedTag = e.target;
     }
 }
-export function initSettings(toggleTimerMode) {
-    settingsButton.addEventListener("click", () => {
-        if (state.timerStarted) return;
-        if (state.stopwatchMode) {
-            countdownDurationContainer.style.display = "none";
-            timerModeDropdown.value = "Stopwatch";
-        } else {
-            countdownDurationContainer.style.display = "flex";
-            timerModeDropdown.value = "Countdown";
+
+function editSessionTag(e) {
+    e.stopPropagation();
+    let tagEdited = false;
+    const editingTag = globals.selectedTag;
+    const oldTag = editingTag.innerHTML;
+    editingTag.contentEditable = "true";
+    editingTag.classList.remove("Selected");
+    editingTag.focus();
+    editingTag.addEventListener("keydown", enterHandler);
+
+    editingTag.addEventListener("blur", escapeHandler);
+    tagSettingContextMenu.style.display = "none";
+
+    function enterHandler(e) {
+        if (e.key == "Enter") {
+            e.preventDefault();
+            tagEdited = true;
+            editingTag.contentEditable = "false";
+            editingTag.blur();
+            globals.tagEditBuffer.Updated.push({
+                old: oldTag,
+                new: editingTag.innerHTML,
+            });
         }
-        countdownDurationMinutes.value =
-            localStorage.getItem("countDownDurationMin") ?? "60";
-        countdownDurationSeconds.value =
-            localStorage.getItem("countDownDurationSec") ?? "00";
-        settingsModal.showModal();
-    });
+    }
+    function escapeHandler() {
+        if (!tagEdited) {
+            editingTag.innerHTML = oldTag;
+            editingTag.contentEditable = "false";
+        }
+        editingTag.removeEventListener("keydown", enterHandler);
+        editingTag.removeEventListener("blur", escapeHandler);
+        globals.selectedTag = null;
+        state.editedTag = false;
+    }
+}
+
+function deleteSessionTag(e) {
+    e.stopPropagation();
+    globals.tagEditBuffer.Deleted.push(globals.selectedTag.innerHTML);
+}
+
+function saveSettings(toggleTimerMode) {
+    if (timerModeDropdown.value == "Countdown") {
+        const min = countdownDurationMinutes.value;
+        const sec = countdownDurationSeconds.value;
+        if (min || sec) {
+            globals.countdownDurationSec = +min * 60 + +sec;
+            // unary + for conversion to int
+            localStorage.setItem("countDownDurationMin", min);
+            localStorage.setItem("countDownDurationSec", sec);
+        }
+        state.stopwatchMode = false;
+    } else {
+        state.stopwatchMode = true;
+    }
+    toggleTimerMode();
+    hideSessionTagInput();
+    settingsModal.close();
+}
+
+export function initSettings(toggleTimerMode) {
+    settingsButton.addEventListener("click", displaySettingsModal);
 
     settingsModalCloseButton.addEventListener("click", () => {
         hideSessionTagInput();
@@ -78,28 +165,11 @@ export function initSettings(toggleTimerMode) {
         toggleCountdownDurationContainer();
     });
 
-    addSessionTagSettingButton.addEventListener("click", () => {
-        const buttonText = addSessionTagSettingButton.innerHTML;
-        if (buttonText == "+") {
-            addSessionTagSettingButton.innerHTML = "x";
-            addSessionTagSettingInput.style.visibility = "visible";
-            addSessionTagSettingInput.focus();
-        } else {
-            hideSessionTagInput();
-        }
-    });
-    addSessionTagSettingInput.addEventListener("keypress", (e) => {
-        if (e.key == "Enter") {
-            const tagDiv = document.createElement("div");
-            tagDiv.innerHTML = addSessionTagSettingInput.value;
-            tagDiv.className = "Session-Tag-Card";
-            sessionTagsList.appendChild(tagDiv);
-            tagsListSettings.appendChild(tagDiv.cloneNode(true));
-            addSessionTagSettingButton.innerHTML = "+";
-            addSessionTagSettingInput.style.visibility = "hidden";
-            addSessionTagSettingInput.value = "";
-        }
-    });
+    addSessionTagSettingButton.addEventListener(
+        "click",
+        showAddSettingTagsInput,
+    );
+    addSessionTagSettingInput.addEventListener("keypress", addSettingTags);
 
     document.addEventListener("click", (e) => {
         if (
@@ -107,9 +177,8 @@ export function initSettings(toggleTimerMode) {
                 "Session-Tag-Card" &&
                     !e.target.closest("Tag-Setting-Context-Menu"),
             )
-        ) {
+        )
             deselectAllCards();
-        }
     });
 
     tagsListSettings.addEventListener("contextmenu", (e) => {
@@ -117,50 +186,11 @@ export function initSettings(toggleTimerMode) {
         showContextMenu(e);
     });
 
-    editTagButton.addEventListener("click", (e) => {
-        e.stopPropagation();
-        const editingTag = globals.selectedTag;
-        const oldTag = editingTag.innerHTML;
-        editingTag.contentEditable = "true";
-        editingTag.classList.remove("Selected");
-        editingTag.focus();
-        editingTag.addEventListener("keypress", function hello(e) {
-            if (e.key == "Enter") {
-                e.preventDefault();
-                editingTag.contentEditable = "false";
-                editingTag.blur();
-                globals.tagEditBuffer.Updated.push({
-                    old: oldTag,
-                    new: editingTag.innerHTML,
-                });
-                editingTag.removeEventListener("keypress", hello);
-                globals.selectedTag = null;
-            }
-        });
-        tagSettingContextMenu.style.display = "none";
-    });
+    editTagButton.addEventListener("click", editSessionTag);
 
-    deleteTagButton.addEventListener("click", (e) => {
-        e.stopPropagation();
-        globals.tagEditBuffer.Deleted.push(globals.selectedTag.innerHTML);
-    });
+    deleteTagButton.addEventListener("click", deleteSessionTag);
 
     settingsModalSaveButton.addEventListener("click", () => {
-        if (timerModeDropdown.value == "Countdown") {
-            const min = countdownDurationMinutes.value;
-            const sec = countdownDurationSeconds.value;
-            if (min || sec) {
-                globals.countdownDurationSec = +min * 60 + +sec;
-                // unary + for conversion to int
-                localStorage.setItem("countDownDurationMin", min);
-                localStorage.setItem("countDownDurationSec", sec);
-            }
-            state.stopwatchMode = false;
-        } else {
-            state.stopwatchMode = true;
-        }
-        toggleTimerMode();
-        hideSessionTagInput();
-        settingsModal.close();
+        saveSettings(toggleTimerMode);
     });
 }
